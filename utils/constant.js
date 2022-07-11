501ea561778356df2b6a6b6846e728558590c198
liangchen25 <liangchen25@iflytek.com>
2022-07-11 19:33:19 +0800

    日历组件

---
+++ utils/constant.js
@@ -0,0 +1,11 @@
+module.exports = {
+  WEEKS: [
+    { id: 1, name: '一' },
+    { id: 2, name: '二' },
+    { id: 3, name: '三' },
+    { id: 4, name: '四' },
+    { id: 5, name: '五' },
+    { id: 6, name: '六' },
+    { id: 7, name: '日' },
+  ]
+}
